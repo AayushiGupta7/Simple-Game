62fc4e27fc48a298bc2845de4b6cd21ddc7c55b8
AayushiGupta7 <76238929+AayushiGupta7@users.noreply.github.com>
2022-06-30 16:58:09 +0530

    Add files via upload

---
+++ Mainn.java
@@ -0,0 +1,9 @@
+public class Mainn {
+
+    public static void main(String[] args) {
+        Player1 player = new Player1("Aayushi ","Sword", 89);
+       System.out.println(player.getName());
+       System.out.println(player.getHealth());
+       System.out.println(player.getWeapon());
+    }
+}
